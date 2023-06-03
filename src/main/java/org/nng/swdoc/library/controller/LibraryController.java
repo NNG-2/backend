1cb9a78be448e06fb3463b390139024176ef8ab6
Maksym Tkachuk <s2m4f6@gmail.com>
2023-06-03 21:10:37 +0300

    Fix typo

--- src/main/java/org/nng/swdoc/library/controller/LibraryController.java
+++ src/main/java/org/nng/swdoc/library/controller/LibraryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/librarie")
+@RequestMapping("/library")
 public class LibraryController {
     @Autowired
     private LibraryService libraryService;
